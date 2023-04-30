e660b7ff2f698f85a1b79a08f2d16bafb2a2d904
Madjic-CH <a.chergui@esi-sba.dz>
2023-04-30 16:51:43 +0100

    updated server middleware to allow react acces

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from usecases.predection import router
 
@@ -18,10 +19,24 @@ def main():
 
 def configure():
     configure_routers()
+    configure_middleware()
 
 
 def configure_routers():
     app.include_router(router)
+
+
+def configure_middleware():
+    origins = [
+        "http://localhost:3000"
+    ]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
 
 if __name__ == "__main__":
