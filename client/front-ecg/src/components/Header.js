e1fa91f2fc2cbca2cf4131cd2f00fa5e1ecb0860
MERYX-bh <jm_baha@esi.dz>
2023-05-05 19:50:05 +0200

    buttons fixed

--- client/front-ecg/src/components/Header.js
+++ client/front-ecg/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Nav } from "react-bootstrap";
 import illHero from '../Assets/header-01.svg'
 import styled from "styled-components";
 
@@ -10,7 +10,7 @@ const Header = () => {
         <Col md={{ span: 6, offset: 0 }} lg={{ span: 4, offset: 1 }}>
           <h1 className='mt-5 text-light font-weight-bold'>Stay healthy with smart ECG</h1>
           <p className='text-light font-weight-light mb-6'>Stay healthy with us, by reguraly verifying your heart rate using ecg deep learning app that predicts if your ECG is normal or abnomarl, What are you waiting to try it out?</p>
-          <Button className="mt-5 p-3 h5">Make predictions</Button>
+          <Nav.Link href="/predict"><Button className="mt-5 p-3 h5">Make predictions</Button></Nav.Link>
         </Col>
         <Col md={{ span: 6, offset: 0}} lg={{ span: 5, offset: 1}}>
           <img src={illHero} style={{height: "30rem"}} alt="header illustration"/>
